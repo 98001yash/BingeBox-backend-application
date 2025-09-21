85b773483b923efce99acbc9c964e75bc582cdeb
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-21 10:27:07 +0530

    created the entity called user entity

---
+++ auth-service/src/main/java/com/company/BingeBox_backend_application/auth_service/entities/User.java
@@ -0,0 +1,36 @@
+package com.company.BingeBox_backend_application.auth_service.entities;
+
+
+import com.company.BingeBox_backend_application.auth_service.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;   // USER, ADMIN
+
+    private boolean enabled = true;   // fot account activation/deactivation
+    private boolean locked = false;  // for account lock
+}
---
+++ auth-service/src/main/java/com/company/BingeBox_backend_application/auth_service/enums/Role.java
@@ -0,0 +1,6 @@
+package com.company.BingeBox_backend_application.auth_service.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
