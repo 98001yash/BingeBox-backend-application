a440e5f33e57b368e2c90934f6e21ecec8fab615
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-21 09:25:24 +0530

    initail commit: added the auth-service, api-gateway and discovery-server

---
+++ auth-service/src/test/java/com/company/BingeBox_backend_application/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.BingeBox_backend_application.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
