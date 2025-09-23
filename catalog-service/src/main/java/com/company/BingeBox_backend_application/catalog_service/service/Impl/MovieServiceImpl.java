8f3d7cbab9de02cad0fadce8788564ccb2ae610f
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-23 07:35:24 +0530

    implemented the createMoviea and getMovieById service in the movieService

---
+++ catalog-service/src/main/java/com/company/BingeBox_backend_application/catalog_service/service/Impl/MovieServiceImpl.java
@@ -0,0 +1,110 @@
+package com.company.BingeBox_backend_application.catalog_service.service.Impl;
+
+import com.company.BingeBox_backend_application.catalog_service.dtos.MovieRequestDto;
+import com.company.BingeBox_backend_application.catalog_service.dtos.MovieResponseDto;
+import com.company.BingeBox_backend_application.catalog_service.entity.*;
+import com.company.BingeBox_backend_application.catalog_service.exceptions.ResourceNotFoundException;
+import com.company.BingeBox_backend_application.catalog_service.repository.*;
+import com.company.BingeBox_backend_application.catalog_service.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+public class MovieServiceImpl implements MovieService {
+
+    private final MovieRepository movieRepository;
+    private final GenreRepository genreRepository;
+    private final ActorRepository actorRepository;
+    private final DirectorRepository directorRepository;
+    private final ProducerRepository producerRepository;
+    private final CategoryRepository categoryRepository;
+    private final ModelMapper modelMapper;
+    @Override
+    public MovieResponseDto createMovie(MovieRequestDto dto) {
+        Movie movie = new Movie();
+
+        movie.setTitle(dto.getTitle());
+        movie.setDescription(dto.getDescription());
+        movie.setThumbnailUrl(dto.getThumbnailUrl());
+        movie.setTrailerUrl(dto.getTrailerUrl());
+        movie.setContentUrl(dto.getContentUrl());
+        movie.setReleaseYear(dto.getReleaseYear());
+        movie.setDuration(dto.getDuration());
+        movie.setCast(dto.getCast());
+        movie.setMaturityRating(dto.getMaturityRating());
+        movie.setFeatured(dto.isFeatured());
+
+        // Relationships
+        if (dto.getGenreIds() != null) {
+            Set<Genre> genres = genreRepository.findAllById(dto.getGenreIds())
+                    .stream()
+                    .collect(Collectors.toSet());
+            movie.setGenres(genres);
+        }
+
+        if (dto.getActorIds() != null) {
+            Set<Actor> actors = actorRepository.findAllById(dto.getActorIds())
+                    .stream()
+                    .collect(Collectors.toSet());
+            movie.setActors(actors);
+        }
+        if (dto.getDirectorIds() != null) {
+            Set<Director> directors = directorRepository.findAllById(dto.getDirectorIds())
+                    .stream()
+                    .collect(Collectors.toSet());
+            movie.setDirectors(directors);
+        }
+
+        if (dto.getProducerIds() != null) {
+            Set<Producer> producers = producerRepository
+                    .findAllById(dto.getProducerIds())
+                    .stream()
+                    .collect(Collectors.toSet());
+            movie.setProducers(producers);
+        }
+
+        if (dto.getCategoryId() != null) {
+            Category category = categoryRepository.findById(dto.getCategoryId())
+                    .orElseThrow(() -> new ResourceNotFoundException("Category not found with id " + dto.getCategoryId()));
+            movie.setCategory(category);
+        }
+
+        Movie savedMovie = movieRepository.save(movie);
+        return convertToResponseDto(savedMovie);
+
+    }
+
+    @Override
+    public MovieResponseDto getMovieById(Long id) {
+       Movie movie = movieRepository.findById(id)
+               .orElseThrow(()->new ResourceNotFoundException("Movie not found with di "+id));
+       return convertToResponseDto(movie);
+    }
+
+    @Override
+    public List<MovieResponseDto> getAllMovies() {
+        return List.of();
+    }
+
+    @Override
+    public MovieResponseDto updateMovie(Long id, MovieRequestDto movieRequestDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteMovie(Long id) {
+
+    }
+
+
+    private MovieResponseDto convertToResponseDto(Movie movie) {
+        return modelMapper.map(movie, MovieResponseDto.class);
+    }
+}
