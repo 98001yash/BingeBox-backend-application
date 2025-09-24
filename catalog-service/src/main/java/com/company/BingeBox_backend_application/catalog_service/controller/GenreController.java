5d342bf285f5f13ffc77ff78b7bbb0447fb9bd97
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-24 21:15:13 +0530

    created the genreController

---
+++ catalog-service/src/main/java/com/company/BingeBox_backend_application/catalog_service/controller/GenreController.java
@@ -0,0 +1,47 @@
+package com.company.BingeBox_backend_application.catalog_service.controller;
+
+
+import com.company.BingeBox_backend_application.catalog_service.dtos.GenreDto;
+import com.company.BingeBox_backend_application.catalog_service.service.GenreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog/genres")
+@RequiredArgsConstructor
+public class GenreController {
+
+    private final GenreService genreService;
+
+    @PostMapping
+    public ResponseEntity<GenreDto> createGenre(@RequestBody GenreDto genreDto){
+        return ResponseEntity.ok(genreService.createGenre(genreDto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GenreDto> getGenreById(@PathVariable Long id){
+        return ResponseEntity.ok(genreService.getGenreById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GenreDto>> getAllCourses(){
+        return ResponseEntity.ok(genreService.getAllGenres());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GenreDto> updateGenre(@PathVariable Long id, @RequestBody GenreDto genreDto) {
+        return ResponseEntity.ok(genreService.updateGenre(id, genreDto));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteGenre(@PathVariable Long id) {
+        genreService.deleteGenre(id);
+        return ResponseEntity.ok("Genre deleted successfully!");
+    }
+
+
+}
