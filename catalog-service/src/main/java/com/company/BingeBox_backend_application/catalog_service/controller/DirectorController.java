df822d3063836720bfcfabfd444022c2957a9956
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-25 13:16:10 +0530

    created and implemented all the API of the directorService

---
+++ catalog-service/src/main/java/com/company/BingeBox_backend_application/catalog_service/controller/DirectorController.java
@@ -0,0 +1,49 @@
+package com.company.BingeBox_backend_application.catalog_service.controller;
+
+
+import com.company.BingeBox_backend_application.catalog_service.dtos.DirectorDto;
+import com.company.BingeBox_backend_application.catalog_service.service.DirectorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/directors")
+@RequiredArgsConstructor
+public class DirectorController {
+
+    private final DirectorService directorService;
+
+    @PostMapping
+    public ResponseEntity<DirectorDto> createDirector(@RequestBody DirectorDto dto) {
+        return new ResponseEntity<>(directorService.createDirector(dto), HttpStatus.CREATED);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DirectorDto> updateDirector(
+            @PathVariable Long id,
+            @RequestBody DirectorDto dto
+    ) {
+        return ResponseEntity.ok(directorService.updateDirector(id, dto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DirectorDto> getDirectorById(@PathVariable Long id) {
+        return ResponseEntity.ok(directorService.getDirectorById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DirectorDto>> getAllDirectors() {
+        return ResponseEntity.ok(directorService.getAllDirectors());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDirector(@PathVariable Long id) {
+        directorService.deleteDirector(id);
+        return ResponseEntity.noContent().build();
+    }
+}
