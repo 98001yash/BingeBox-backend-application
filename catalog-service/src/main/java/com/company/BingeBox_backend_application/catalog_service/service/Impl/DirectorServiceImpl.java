58adb65a9a9ca2af0ff6aae20259e74803401eca
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-25 13:10:21 +0530

    implemented all the service of the director

--- catalog-service/src/main/java/com/company/BingeBox_backend_application/catalog_service/service/Impl/DirectorServiceImpl.java
+++ catalog-service/src/main/java/com/company/BingeBox_backend_application/catalog_service/service/Impl/DirectorServiceImpl.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -23,22 +24,35 @@ public class DirectorServiceImpl implements DirectorService {
 
     @Override
     public DirectorDto updateDirector(Long id, DirectorDto dto) {
-        return null;
+        Director director = directorRepository.findById(id)
+                .orElseThrow(()->new RuntimeException("Director not found"));
+
+        director.setName(dto.getName());
+        director.setProfileImageUrl(dto.getProfileImageUrl());
+
+        return mapToDto(directorRepository.save(director));
     }
 
     @Override
     public DirectorDto getDirectorById(Long id) {
-        return null;
+        Director director = directorRepository.findById(id)
+                .orElseThrow(()->new RuntimeException("Director not found"));
+        return mapToDto(director);
     }
 
     @Override
     public List<DirectorDto> getAllDirectors() {
-        return List.of();
+        return directorRepository.findAll().stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public void deleteDirector(Long id) {
-
+        if(!directorRepository.existsById(id)){
+            throw new RuntimeException("Director not found");
+        }
+        directorRepository.deleteById(id);
     }
 
     private DirectorDto mapToDto(Director director) {
