5b59aaa9e7e688e45153ee3e787759edb9396dd7
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-25 21:22:09 +0530

    setting up for the searchand filter in the catalog-service

--- catalog-service/src/main/java/com/company/BingeBox_backend_application/catalog_service/repository/MovieRepository.java
+++ catalog-service/src/main/java/com/company/BingeBox_backend_application/catalog_service/repository/MovieRepository.java
@@ -2,6 +2,7 @@ package com.company.BingeBox_backend_application.catalog_service.repository;
 
 import com.company.BingeBox_backend_application.catalog_service.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface MovieRepository extends JpaRepository<Movie,Long> {
+public interface MovieRepository extends JpaRepository<Movie,Long>, JpaSpecificationExecutor<Movie>  {
 }
