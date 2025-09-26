4a70a984bdc9e26e9e9c1faaad65be150be996f3
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-26 13:11:38 +0530

    created the watchListServiceImpl class and implemented the required methods

---
+++ user-service/src/main/java/com/company/BingeBox_backend_application/user_service/service/Impl/WatchlistServiceImpl.java
@@ -0,0 +1,75 @@
+package com.company.BingeBox_backend_application.user_service.service.Impl;
+
+import com.company.BingeBox_backend_application.user_service.dtos.WatchlistItemDto;
+import com.company.BingeBox_backend_application.user_service.entities.UserProfile;
+import com.company.BingeBox_backend_application.user_service.entities.WatchlistItem;
+import com.company.BingeBox_backend_application.user_service.exceptions.ResourceNotFoundException;
+import com.company.BingeBox_backend_application.user_service.repository.UserProfileRepository;
+import com.company.BingeBox_backend_application.user_service.repository.WatchlistRepository;
+import com.company.BingeBox_backend_application.user_service.service.WatchlistService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class WatchlistServiceImpl implements WatchlistService {
+
+    private final WatchlistRepository watchlistRepository;
+    private final UserProfileRepository userProfileRepository;
+
+    @Override
+    public WatchlistItemDto addToWatchList(Long userId, Long contentId, String contentType) {
+       log.info("Adding contentId={} contentType={} to watchList for userId={}", contentId, contentType, userId);
+
+       UserProfile user = userProfileRepository.findById(userId)
+               .orElseThrow(()->new ResourceNotFoundException("User not found with id: "+userId));
+
+        WatchlistItem item = WatchlistItem.builder()
+                .contentId(contentId)
+                .contentType(contentType)
+                .user(user)
+                .build();
+
+        WatchlistItem saved = watchlistRepository.save(item);
+
+        log.info("Successfully added contentId={} to userId={} watchlist", contentId, userId);
+        return mapToDto(saved);
+
+    }
+
+    @Override
+    public void removeFromWatchlist(Long userId, Long contentId, String contentType) {
+        log.info("Removing contentId={} contentType={} from watchlist for userId={}", contentId, contentType, userId);
+
+        WatchlistItem item = watchlistRepository.findByUser_UserIdAndContentIdAndContentType(userId, contentId, contentType)
+                .orElseThrow(() -> new ResourceNotFoundException("Watchlist item not found for contentId=" + contentId));
+
+        watchlistRepository.delete(item);
+        log.info("Removed contentId={} from userId={} watchlist", contentId, userId);
+    }
+
+
+    @Override
+    public List<WatchlistItemDto> getUserWatchlist(Long userId) {
+        log.info("Fetching watchlist for userId={}", userId);
+
+        List<WatchlistItem> items = watchlistRepository.findByUser_UserId(userId);
+
+        return items.stream().map(this::mapToDto).collect(Collectors.toList());
+    }
+
+
+
+    private WatchlistItemDto mapToDto(WatchlistItem item) {
+        return WatchlistItemDto.builder()
+                .id(item.getId())
+                .contentId(item.getContentId())
+                .contentType(item.getContentType())
+                .build();
+    }
+}
