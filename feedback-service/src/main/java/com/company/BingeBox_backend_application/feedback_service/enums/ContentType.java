2dfa59c42710d48f50bda46f3d28669df4df42f8
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-29 02:12:37 +0530

    added the enums called contentType and feedbackType

---
+++ feedback-service/src/main/java/com/company/BingeBox_backend_application/feedback_service/enums/ContentType.java
@@ -0,0 +1,8 @@
+package com.company.BingeBox_backend_application.feedback_service.enums;
+
+public enum ContentType {
+
+    MOVIE,
+    SERIES,
+    DOCUMENTARY
+}
---
+++ feedback-service/src/main/java/com/company/BingeBox_backend_application/feedback_service/enums/FeedbackType.java
@@ -0,0 +1,10 @@
+package com.company.BingeBox_backend_application.feedback_service.enums;
+
+public enum FeedbackType {
+
+    LIKE,
+    DISLIKE,
+    LOVE,
+    RATING,
+    COMMENT
+}
